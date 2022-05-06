61417321ded1dcd052b6ecd021969ba1d5d8dd69
Walter Tanaka Chifamba <63348127+tanakawale@users.noreply.github.com>
2022-05-06 20:32:40 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function getNumberOrString(value) {
+    // Convert a string value to a number if possible
+    let number_value = Number(value);
+    if (Number.isNaN(number_value)) {
+      return value
+    } else {
+      return number_value
+    }
+  }
+  
+  
+  
+  document.getElementById('submit').addEventListener('click', (event) => {
+    let element_list = document.getElementById('list');
+    element_list.innerText = getNumberOrString(document.getElementById('text').value);
+  
+  });
